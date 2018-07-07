fe6a1abe1ac3f3e9cbfe893d0e92e4f60ce0a37d
mldn <mldnqa@163.com>
2018-07-07 09:32:23 +0800

    Flume-Kafka整合。

---
+++ mldnkafka-flume/src/main/java/org/mldnkafka/flume/TestFlume.java
@@ -0,0 +1,12 @@
+package org.mldnkafka.flume;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestFlume {
+	private static final Logger LOG = LoggerFactory.getLogger(TestFlume.class) ;
+	public static void main(String[] args) {
+		System.out.println("中午田奇伟请大家吃冰棍，作为它的惩罚，每人标准￥0.5");
+		LOG.info("[************* www.mldn.cn ****************]");
+	}
+}
